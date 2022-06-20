d8f8661b5bfd89466e2dfc07ab37316466d592a4
Abdelfattah <hilmiabdelfattah@gmail.com>
2022-06-20 20:53:56 +0100

    Bug Fixes

--- core/serializers.py
+++ core/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', 'password']
+        fields = ['id', 'email', 'user_name', 'password']
         extra_kwargs = {
             'password': {
                 'write_only':True
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self,validated_data):
-        password = validated_data('password',None)
+        password = validated_data.pop('password',None)
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
-from rest_framework.generics import CreateAPIView
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+
 
 class UserRegister(CreateAPIView):
+    # permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
