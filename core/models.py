87da641533091afac1ff23ddf6677dcab37872af
Abdelfattah <hilmiabdelfattah@gmail.com>
2022-06-30 17:15:42 +0100

    typo in user object manager

--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ class CustomAccountManager(BaseUserManager):
     def create_superuser(self, email, user_name, password, **other_fields):
         other_fields.setdefault('is_staff',True)
         other_fields.setdefault('is_superuser',True)
-        other_fields.setdefault('is_activw',True)
+        other_fields.setdefault('is_active',True)
 
         if other_fields.get('is_staff') is not True:
             raise ValueError(
