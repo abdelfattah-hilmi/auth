056ec9dd85488a2f73c91659dce9c0534da25a53
Abdelfattah <hilmiabdelfattah@gmail.com>
2022-06-30 13:12:23 +0100

    Added logout View

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,14 @@ from .views import UserRegister, DevicesListView, DeviceView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView
 )
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/',UserRegister.as_view(),name='Users registration Endpoint'),
-    path('devices/',DevicesListView.as_view(),name='List of all devices'),
-    path('devices/<str:id>',DeviceView.as_view(),name='Device details')
+    path('logout/',TokenBlacklistView.as_view(), name='logout_view'),
+    path('register/',UserRegister.as_view(),name='Users_registration_Endpoint'),
+    path('devices/',DevicesListView.as_view(),name='List_of_all_devices'),
+    path('devices/<str:id>',DeviceView.as_view(),name='Device_details'),
 ]
